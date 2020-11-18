44b6813e8d06047ac9713f9a2a261fb736094851
AdamTMalek <malekadas@gmail.com>
2020-11-18 20:45:44 +0000

    Add function for the attribute selection based on correlation

--- src/weka.py
+++ src/weka.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 from pathlib import Path
 
@@ -10,7 +11,26 @@ def set_class_attr_to_nominal(input_file_path: Path, output_file_path: Path, wek
 
     :param input_file_path: CSV or ARFF file
     :param output_file_path: ARFF output file with the applied filter
-    :param weka_jar_path: Path to the weka's jar
+    :param weka_jar_path: Path to the Weka's jar
     """
     subprocess.call(['java', '-cp', weka_jar_path, 'weka.filters.unsupervised.attribute.NumericToNominal',
                      '-R', 'last', '-i', input_file_path, '-o', output_file_path])
+
+
+def select_top_correlating_attrs(input_file_path: Path, output_file_path: Path, attributes_to_take: int,
+                                 weka_jar_path: Path):
+    """
+    Applies Weka's attribute selection using CorrelationAttributeEval evaluator.
+
+    :param input_file_path: CSV or ARFF file
+    :param output_file_path: ARFF output file
+    :param attributes_to_take: Number of attributes to take
+    :param weka_jar_path: Path to the Weka's jar
+    :return:
+    """
+    RANKER_THRESHOLD = '-1.7976931348623157E308'  # Picked by Weka automatically
+    search_method = f'weka.attributeSelection.Ranker -T {RANKER_THRESHOLD} -N {attributes_to_take}'
+    command = ' '.join(['java', '-cp', str(weka_jar_path), 'weka.filters.supervised.attribute.AttributeSelection',
+                        '-E', 'weka.attributeSelection.CorrelationAttributeEval', '-S', f'"{search_method}"',
+                        '-i', str(input_file_path), '-o', str(output_file_path)])
+    os.system(command)
