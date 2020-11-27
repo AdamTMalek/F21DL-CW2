6a63204f43d8171cf329ac47eb559b474aed753e
Chris Hulme <chris_hulme99@outlook.com>
2020-11-27 16:14:51 +0000

    Changed evidence file names

--- src/visualise_output.py
+++ src/visualise_output.py
@@ -44,7 +44,7 @@ def draw_value_labels(bar,ax):
             xy=(rect.get_x() + rect.get_width() / 2, height),
             xytext=(0, 3),  # 3 points vertical offset
             textcoords="offset points",
-            ha='center', va='bottom', fontsize=6)
+            ha='center', va='bottom', fontsize=4)
  
 
 def annotate_j48(plt):
@@ -59,33 +59,44 @@ def annotate_j48(plt):
 
 
 j48,randForest = read_config_csv()
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
 
-# Plot results of different J48 Configurations
-j48plt, j48ax = bar_chart(j48.iloc[:,5:],"Comparison of J48 Configurations", "Configurations", np.arange(15))
-j48plt = annotate_j48(j48plt)
-j48plt.show()
+# # Plot results of different J48 Configurations
+# j48plt, j48ax = bar_chart(j48.iloc[:,5:],"Comparison of J48 Configurations", "Configurations", np.arange(15))
+# j48plt = annotate_j48(j48plt)
+# j48plt.show()
 
-# Plot results of changing number of iterations
-iterationLabels = np.sort(np.unique(randForest.loc[:,'NumIteration']))
-RFIterations = randForest.iloc[6:15,:].sort_values('NumIteration').loc[:,'TP Rate':]
-rPlt, rAx = bar_chart(RFIterations,"Changing Number of Iterations of Random Forest","Iterations",iterationLabels)
-rPlt.show()
+# # Plot results of changing number of iterations
+# iterationLabels = np.sort(np.unique(randForest.loc[:,'NumIteration']))
+# RFIterations = randForest.iloc[6:15,:].sort_values('NumIteration').loc[:,'TP Rate':]
+# rPlt, rAx = bar_chart(RFIterations,"Changing Number of Iterations of Random Forest","Iterations",iterationLabels)
+# rPlt.show()
 
 
-# Plot results of changing max depth of Random Forest
-maxDepthLabels = np.sort(np.unique(randForest.loc[:,'Max Depth']))
-RFMaxDepth = randForest.iloc[22:,:].sort_values('Max Depth').loc[:,'TP Rate':]
-rPlt, rAx = bar_chart(RFMaxDepth,"Changing Max Depth of Random Forest","Max Depth",maxDepthLabels[1:])
-rPlt.show()
+# # Plot results of changing max depth of Random Forest
+# maxDepthLabels = np.sort(np.unique(randForest.loc[:,'Max Depth']))
+# RFMaxDepth = randForest.iloc[22:,:].sort_values('Max Depth').loc[:,'TP Rate':]
+# rPlt, rAx = bar_chart(RFMaxDepth,"Changing Max Depth of Random Forest","Max Depth",maxDepthLabels[1:])
+# rPlt.show()
 
-# Plot results of changing bag size percentage
-bagSizeLabels = np.sort(np.unique(randForest.loc[:,'Bag Size Percent']))
-RFMaxDepth = randForest.iloc[14:18,:].sort_values('Bag Size Percent').loc[:,'TP Rate':]
-rPlt, rAx = bar_chart(RFMaxDepth,"Changing Bag Size Percentage of Random Forest","Bag Size Percentage (%)",bagSizeLabels[1:],value_labels=True)
-rPlt.show()
+# # Plot results of changing bag size percentage
+# bagSizeLabels = np.sort(np.unique(randForest.loc[:,'Bag Size Percent']))
+# RFMaxDepth = randForest.iloc[14:18,:].sort_values('Bag Size Percent').loc[:,'TP Rate':]
+# rPlt, rAx = bar_chart(RFMaxDepth,"Changing Bag Size Percentage of Random Forest","Bag Size Percentage (%)",bagSizeLabels[1:],value_labels=True)
+# rPlt.show()
 
-# Plot results of changing seed
-seedLabels = np.sort(np.unique(randForest.loc[:,'Seed']))
-RFSeed = randForest.iloc[18:22,:].sort_values('Seed').loc[:,'TP Rate':]
-rPlt, rAx = bar_chart(RFSeed,"Changing Seed of Random Forest","Seed",bagSizeLabels[1:],value_labels=True)
+# # Plot results of changing seed
+# seedLabels = np.sort(np.unique(randForest.loc[:,'Seed']))
+# RFSeed = randForest.iloc[18:22,:].sort_values('Seed').loc[:,'TP Rate':]
+# rPlt, rAx = bar_chart(RFSeed,"Changing Seed of Random Forest","Seed",seedLabels[1:],value_labels=True)
+# rPlt.show()
+
+# Plot results of changing number of randomly chosen features
+numFeaturesLabels = [0,5,20,30,40,50]
+RFSeed = randForest.iloc[0:6,:].sort_values('NumFeatures').loc[:,'TP Rate':]
+print(RFSeed)
+rPlt, rAx = bar_chart(RFSeed,"Changing number of randomly chosen features of Random Forest","Number of Features",numFeaturesLabels,value_labels=True)
 rPlt.show()
